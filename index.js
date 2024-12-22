950a7ea79e6d98fa086a0820eae68a31d302b418
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2024-12-23 00:03:17 +0530

    put api added

--- index.js
+++ index.js
@@ -49,6 +49,25 @@ app.delete("/books/:name", (req, res) => {
         data: { Books },
     });
 });
+app.put("/book",(req,res)=>{
+    const {name,price}=req.body;
+    for(let i=0;i<Books.length;i++){
+        if(name==Books[i].name){
+            Books[i].name=name;
+            Books[i].price=price;
+            return res.send({
+                success:true,
+                msg :"book info updated",
+                data:Books[i]
+            })
+        }
+
+    }
+
+});
+app.get("/health",(req,res)=>{
+    return res.send("server is up and running")
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on ${PORT}`);
