a5c3dc9624b41f701b441926f279524c70489ef1
AditiNaldurgkar <aditinaldurgkar03@gmail.com>
2025-12-19 15:34:52 +0530

    port changes

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import Book from "./models/Book.js";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 mongoose.connect(process.env.MONGO_URI)
